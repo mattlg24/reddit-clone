799e53bb5817459fa68502ae923be87f76b2308d
Matt Gardner <matthewlgardner@gmail.com>
2016-10-24 12:05:15 -0600

    post form shows and hides on submit post button

--- app.js
+++ app.js
@@ -3,10 +3,13 @@ const app = angular.module('redditCloneApp', [])
 app.controller('MainController', ['$scope', function($scope) {
 
     $scope.posts = []
-    $scope.posts = {}
+    $scope.post = {}
 
-    $scope.submitPostForm = function() {
+    $scope.submitPostForm = function(post) {
         event.preventDefault()
+        console.log('post info', post);
+        $scope.posts.push($scope.post)
+        $scope.post = ''
 
 
     }
