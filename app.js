210ce2c51f99a023bd749f6edb4ffef19663c11e
Matt Gardner <matthewlgardner@gmail.com>
2016-10-26 13:21:28 -0600

    added moment js for date

--- app.js
+++ app.js
@@ -9,10 +9,14 @@ app.controller('MainController', ['$scope', '$log', function($scope, $log) {
         title: 'Halloween',
         author: 'Dracula',
         imageUrl: 'http://www.wikihow.com/images/e/ee/Colored-Intro-7.jpg',
-        description: 'Spooky Scary Creepy Crawly. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+        description: 'Spooky Scary Creepy Crawly. Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
         votes: 1,
-        date: 'October 24, 2016',
-        comments: []
+        date: new Date('October 24, 2016'),
+        momentDate: moment().subtract(2, 'days').calendar(),
+        comments: [{
+            author: 'Ghost',
+            text: 'You are scary'
+        }]
     }, {
         id: 1,
         title: 'Christmas',
@@ -20,14 +24,20 @@ app.controller('MainController', ['$scope', '$log', function($scope, $log) {
         imageUrl: 'http://coloringkids.org/wp-content/uploads/santa-cartoon.jpg',
         description: 'I like to bring presents to everyone because I\'m a jolly good fellow.',
         votes: 0,
-        date: 'October 25, 2016',
-        comments: []
+        date: new Date('October 25, 2016'),
+        momentDate: moment().subtract(1, 'day').calendar(),
+        comments: [{
+            author: 'Rudolph',
+            text: 'I don\'t want to pull the sleigh anymore'
+        }, {
+            author: 'Blitzen',
+            text: 'Rudolph is a ninkampoop'
+        }]
     }]
 
     $scope.post = {}
     $scope.commentForm = {};
-    $scope.post.comments = []
-        // $scope.date = ''
+    // $scope.date = ''
 
     function createNewId() {
         let newId = $scope.view.posts.length
@@ -36,11 +46,11 @@ app.controller('MainController', ['$scope', '$log', function($scope, $log) {
     }
 
     $scope.downVote = function(post) {
-        post.votes -= 1
+        post.votes--
     }
     $scope.upVote = function(post) {
         // console.log('post', post);
-        post.votes += 1
+        post.votes++
     }
 
     $scope.submitPostForm = function() {
@@ -49,22 +59,31 @@ app.controller('MainController', ['$scope', '$log', function($scope, $log) {
         $scope.post.id = createNewId()
         $scope.post.votes = 0
         $scope.post.date = new Date()
+        $scope.post.momentDate = moment().calendar()
+        $scope.post.comments = []
         $scope.view.posts.push($scope.post)
         $scope.post = ''
         $scope.postForm.$setPristine()
-        $scope.comments = 2
     }
 
+
+
     $scope.submitCommentForm = function(post) {
+        console.log('view.posts.length is', $scope.view.posts.length);
+        for (var i = 0; i < $scope.view.posts.length; i++) {
+            let numComments = $scope.view.posts[i].comments.length
+            console.log('num comments is', numComments);
+        }
         // console.log('post is', post);
         // $log.info('all posts...............', $scope.posts)
         event.preventDefault()
-            // console.log('postid', post.id);
+        console.log('postid', post.id);
         let id = post.id
         var newComment = angular.copy($scope.commentForm)
             // console.log('newComment is', newComment);
         $scope.view.posts[id].comments.push(newComment)
         post.showCommentForm = false
+        this.commentSection = true
     }
 
     $scope.orderVal = '-votes'
